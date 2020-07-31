fbb79c039b1c78dcece65e474ba0c04b04127eb6
Akanksha Bhavsar <65349267+akankshaa02@users.noreply.github.com>
2020-07-31 17:44:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function setup() {
     frameRate(8);
     
     //create link function
-   link = createA("https://akankshaa02.github.io/pacman.github.io/", 
+   link = createA("https://akankshaa02.github.io/PacMan/", 
                        "click", "_blank");
 
    // Posotion the anchor objects 
